1a0fb9e0d5d8c0cf9ba0727d7f12324b7385af3b
miao <miao@DESKTOP-7IH56EN>
2026-03-12 16:31:29 +0800

    做了一个测试DEMO

---
+++ ZrosSmoke/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ZROS.Core;
+using System;
+
+Console.WriteLine("Hello, World!");
+
+using var context = RosContext.Create();
+Console.WriteLine($"IsOpen={context.IsOpen}, IsSimulated={context.IsSimulated}");
+
+using var node = context.CreateNode("smoke_node");
+Console.WriteLine($"Node: {node.Name} ns={node.Namespace}");
